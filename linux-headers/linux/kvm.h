f7a7c482a931f9dc7b98f3a110e04cab3333357f
Adonis <050620@sjtu.edu.cn>
2026-05-19 06:36:52 +0000

    Modify the number of KVM_CAP_X86_DSM

--- linux-headers/linux/kvm.h
+++ linux-headers/linux/kvm.h
@@ -973,7 +973,7 @@ struct kvm_enable_cap {
 #define KVM_CAP_ARM_EL2 240
 #define KVM_CAP_ARM_EL2_E2H0 241
 #define KVM_CAP_RISCV_MP_STATE_RESET 242
-#define KVM_CAP_X86_DSM 245
+#define KVM_CAP_X86_DSM 249
 
 struct kvm_irq_routing_irqchip {
 	__u32 irqchip;
